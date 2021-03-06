587e90ca137858249043e101b1d963181615e3f1
Manali Khanna <kmanali@thoughtworks.com>
2021-03-06 18:57:31 +0530

    Updated Provider config to be able to run on jenkins

--- platform/terraform/provider.tf
+++ platform/terraform/provider.tf
@@ -1,6 +1,7 @@
 provider "kubernetes" {
-  config_path    = "~/.kube/config"
-  config_context = "minikube"
+  host = "https://192.168.64.12:8443"
 
+  client_certificate     = "${file("~/.kube/client-cert.pem")}"
+  client_key             = "${file("~/.kube/client-key.pem")}"
+  cluster_ca_certificate = "${file("~/.kube/client-ca-cert.pem")}"
 }
-
